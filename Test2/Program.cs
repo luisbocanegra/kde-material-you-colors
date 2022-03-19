6130f6e7c0a840237f62ff656f0d0a308026fd45
luisbocanegra <luisbocanegra17b@gmail.com>
2022-03-19 16:00:46 -0600

    add output format for commandline

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -6,10 +6,10 @@ using MaterialColorUtilities.Utils;
 
 using MaterialColorUtilities.Palettes;
 using MaterialColorUtilities.Schemes;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+
 using System.Drawing;
 using System.Text;
+using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 // --- Console.WriteLine("Hello, World!");
 
@@ -22,25 +22,25 @@ class Program
     static Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(imageResourceId)!;
     static SKBitmap bitmap = SKBitmap.Decode(resourceStream).Resize(new SKImageInfo(112, 112), SKFilterQuality.Medium);
     static int[] pixels = bitmap.Pixels.Select(p => (int)(uint)p).ToArray();
-    
+
     // This is where the magic happens
     static int seedColor = ImageUtils.ColorFromImage(pixels);
-    
+
     // CorePalette gives you access to every tone of the key colors
     static CorePalette corePalette = CorePalette.Of(seedColor);
 
     // override some colors of the Light scheme ;)
-    class MyLightScheme : LightScheme 
+    class MyLightScheme : LightScheme
     {
 
         public MyLightScheme(CorePalette corePalette) : base(corePalette) { }
         //string s = "String from MyScheme";
 
-        protected override int BackgroundLight => corePalette.Primary[1000];
+        protected override int BackgroundLight => corePalette.Neutral[99];
     }
 
     // override some colors of the Light scheme ;)
-    class MyDarkScheme : DarkScheme 
+    class MyDarkScheme : DarkScheme
     {
 
         public MyDarkScheme(CorePalette corePalette) : base(corePalette) { }
@@ -52,37 +52,50 @@ class Program
     // Main Method
     static void Main()
     {
-        
+        // Create custom schemes
         MyLightScheme lightScheme = new(corePalette);
         MyDarkScheme darkScheme = new(corePalette);
 
-        // 'E' is object of class stream
-        // also works as object of
-        // class 'web'
-        //MyScheme E = MyScheme;
-        //LightSchem<int> scheme = new LightScheme(corePalette);
+        // Save color palette for export
+        var colors = new Dictionary<string, string>();
 
+        // light section of export
+        var colors_light = new Dictionary<string, string>();
         foreach (var property in typeof(Scheme<int>).GetProperties())
         {
-            // var colors_light = new Dictionary<string, string>();
             int color = (int)property.GetValue(lightScheme)!;
-            Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
-            //colors_light.Add(property.Name, "#" + color.ToString("X")[2..]);
+            //Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
+            colors_light.Add(property.Name, "#" + color.ToString("X")[2..]);
         }
 
+        // dark section of export
+        var colors_dark = new Dictionary<string, string>();
         foreach (var property in typeof(Scheme<int>).GetProperties())
         {
-            // var colors_light = new Dictionary<string, string>();
             int color = (int)property.GetValue(darkScheme)!;
-            Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
-            //colors_light.Add(property.Name, "#" + color.ToString("X")[2..]);
+            //Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
+            colors_dark.Add(property.Name, "#" + color.ToString("X")[2..]);
         }
 
-        Console.WriteLine("aa");
-        // it first invokes 'showdata()'
-        // of class 'web' then it invokes
-        // 'showdata()' of class 'stream'
-        //E.showdata();
+        // light to json
+        string jsonLight = JsonConvert.SerializeObject(colors_light);
+        //string jsonLightUnescaped = Regex.Unescape(jsonLight);
+        //colors.Add("light", jsonLightUnescaped);
+
+        // dark to json
+        string jsonDark = JsonConvert.SerializeObject(colors_dark);
+        // string jsonDarkUnescaped = Regex.Unescape(jsonDark);
+        // colors.Add("dark", jsonDarkUnescaped);
+
+        //string jsonString = JsonConvert.SerializeObject(colors_light);
+
+        //jsonString = Regex.Unescape(jsonString);
+        //Console.WriteLine(jsonString);
+        Console.Write("{\"light\":");
+        Console.WriteLine(jsonLight+",");
+        Console.WriteLine("\"dark\":");
+        Console.WriteLine(jsonDark);
+        Console.WriteLine("}");
 
     }
 }
--- main.py
+++ main.py
@@ -10,17 +10,23 @@ import shutil
 import pretty_errors
 
 url = "http://localhost/kde-material-you-colors/"
+
+
 def remove_last_line_from_string(s):
     return s[:s.rfind('\n')]
+
+
 # Set current wallpaper
-wallpaper_path = subprocess.Popen("cat '/home/luis/.config/plasma-org.kde.plasma.desktop-appletsrc' | sed '75q;d' | sed 's#Image=file://###'", shell=True,stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
-#print(wallpaper_path)
+wallpaper_path = subprocess.Popen("cat '/home/luis/.config/plasma-org.kde.plasma.desktop-appletsrc' | sed '74q;d' | sed 's#Image=file://###'",
+                                  shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
+# print(wallpaper_path)
 #wallpaper_path = wallpaper_path.split('\n', 1)[1]
-copy_command = "cp -rfL '"+ wallpaper_path + "' Test2/Resources/wallpaper.png"
+copy_command = "cp -rfL '" + wallpaper_path + "' Test2/Resources/wallpaper.png"
 print(copy_command)
 os.system(copy_command)
 
-colors_from_net = subprocess.Popen("cd /run/media/luis/Windows10/Users/luis/Documents/kde-material-you-colors/Test2 && dotnet run", shell=True,stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
+colors_from_net = subprocess.Popen("cd /run/media/luis/Windows10/Users/luis/Documents/kde-material-you-colors/Test2 && dotnet run",
+                                   shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
 
 print(colors_from_net)
 
@@ -29,15 +35,17 @@ if colors_from_net:
     # get text from element
     colors_str = colors_from_net
 
-    #print(colors_str)
+    # print(colors_str)
 
     # parse colors string to json
     colors_json = json.loads(colors_str)
 
-    with open ('putput.json', 'w', encoding='utf8') as light_scheme_file:
-            light_scheme_file.write(str(colors_json).replace("'", '"'))
+    with open('putput.json', 'w', encoding='utf8') as light_scheme_file:
+        # light_scheme_file.write(str(colors_json))
+        light_scheme_file.write(json.dumps(
+            colors_json, indent=4, sort_keys=False))
 
-    #print(colors_json)
+    # print(colors_json)
     colors_light = ColorScheme(colors_json)
     colors_light.make_light_scheme()
 else:
