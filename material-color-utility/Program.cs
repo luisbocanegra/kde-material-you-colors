7b7c72ff3b05bdd0730a3b7beb7fa22efcb981b1
luisbocanegra <luisbocanegra17b@gmail.com>
2022-03-22 21:09:20 -0600

    .NET rename project and enable Compression for release

---
+++ material-color-utility/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SkiaSharp;
+using MaterialColorUtilities.Utils;
+using MaterialColorUtilities.Palettes;
+using MaterialColorUtilities.Schemes;
+using Newtonsoft.Json;
+
+class Program
+{
+    //Make temporary color palette
+    static int tempColor = -452997;
+    public static CorePalette corePalette = CorePalette.Of(tempColor);
+    
+    // override some colors of the Light scheme ;)
+    class MyLightScheme : LightScheme
+    {
+
+        public MyLightScheme(CorePalette corePalette) : base(corePalette) { }
+        //string s = "String from MyScheme";
+
+        protected override int BackgroundLight => corePalette.NeutralVariant[95];
+    }
+
+    // override some colors of the Light scheme ;)
+    class MyDarkScheme : DarkScheme
+    {
+
+        public MyDarkScheme(CorePalette corePalette) : base(corePalette) { }
+        //string s = "String from MyScheme";
+
+        protected override int BackgroundDark => corePalette.NeutralVariant[10];
+    }
+
+    // Main Method
+    static void Main(string[] args)
+    {
+        // Generate seed color from an image
+    
+        // Load the image from passed argument with absolute path
+        string path = string.Join('-',args);
+        // Open filestream of image as read only
+        FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read);
+
+        // Then decode and resize using SkiaSharp.
+        SKImage img = SKImage.FromEncodedData(fs);
+        SKBitmap bitmap = SKBitmap.FromImage(img).Resize(new SKImageInfo(112, 112), SKFilterQuality.Medium);
+
+        int[] pixels = bitmap.Pixels.Select(p => (int)(uint)p).ToArray();
+
+        // This is where the magic happens
+        int seedColor = ImageUtils.ColorFromImage(pixels);
+
+        // CorePalette gives you access to every tone of the key colors
+        CorePalette myCorePalette = CorePalette.Of(seedColor);
+
+        // Create custom schemes
+        MyLightScheme lightScheme = new(myCorePalette);
+        MyDarkScheme darkScheme = new(myCorePalette);
+
+        //TODO: Save color palette to single json
+        // var colors = new Dictionary<string, string>();
+
+        // light section of export
+        var colors_light = new Dictionary<string, string>();
+        foreach (var property in typeof(Scheme<int>).GetProperties())
+        {
+            int color = (int)property.GetValue(lightScheme)!;
+            //Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
+            colors_light.Add(property.Name, "#" + color.ToString("X")[2..]);
+        }
+
+        // dark section of export
+        var colors_dark = new Dictionary<string, string>();
+        foreach (var property in typeof(Scheme<int>).GetProperties())
+        {
+            int color = (int)property.GetValue(darkScheme)!;
+            //Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
+            colors_dark.Add(property.Name, "#" + color.ToString("X")[2..]);
+        }
+
+        // light to json
+        string jsonLight = JsonConvert.SerializeObject(colors_light);
+
+        // dark to json
+        string jsonDark = JsonConvert.SerializeObject(colors_dark);
+
+        Console.Write("{\"light\":");
+        Console.WriteLine(jsonLight + ",");
+        Console.WriteLine("\"dark\":");
+        Console.WriteLine(jsonDark);
+        Console.WriteLine("}");
+    }
+}
