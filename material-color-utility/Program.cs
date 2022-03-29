eda9e1f3df8b5d3fd731b3e5477e27ec885b5422
luisbocanegra <luisbocanegra17b@gmail.com>
2022-03-29 01:04:58 -0600

    .NET: return best colors instead of only one and make it user selectable, #3

--- material-color-utility/Program.cs
+++ material-color-utility/Program.cs
@@ -1,19 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SkiaSharp;
-using MaterialColorUtilities.Utils;
 using MaterialColorUtilities.Palettes;
 using MaterialColorUtilities.Schemes;
+using MaterialColorUtilities.Quantize;
 using Newtonsoft.Json;
 
 class Program
 {
+    // Return a list of 1+ best colors
+    public static List<int> ColorsFromImage(int[] pixels)
+    {
+        var result = QuantizerCelebi.Quantize(pixels,128);
+        var ranked = new List<int>(Custom.Scorer.Score(result));
+        //var top = ranked.First();
+        return ranked;
+    }
     // Main Method
     static void Main(string[] args)
     {
         // Generate seed color from an image
-    
         // Load the image from passed argument with absolute path
-        string path = string.Join('-',args);
+        string path = args[0];
+        // get the alt color argument or default to 0 if not given
+        int altColor = 0;
+        if (args.Length > 1)
+        {
+            altColor = int.Parse(args[1]);
+        }
+        
         // Open filestream of image as read only
         FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read);
 
@@ -24,7 +38,24 @@ class Program
         int[] pixels = bitmap.Pixels.Select(p => (int)(uint)p).ToArray();
 
         // This is where the magic happens
-        int seedColor = ImageUtils.ColorFromImage(pixels);
+        // Get a list from the best colors that will at least contain one element
+        var seedColors = ColorsFromImage(pixels);
+        
+        int totalColors=seedColors.Count;
+        int seedColor,seedNo = 0;
+        // If the requested index is less or equal than the lenght of the colors list 
+        if (totalColors > altColor)
+        {
+            // get the color from the list
+            seedColor = seedColors[altColor];
+            seedNo = altColor;
+            
+        }else
+        // if the requested index is larger than the colors index use the last one
+        {
+            seedColor = seedColors[totalColors-1];
+            seedNo = totalColors-1;
+        }
 
         // CorePalette gives you access to every tone of the key colors
         CorePalette myCorePalette = CorePalette.Of(seedColor);
@@ -37,11 +68,17 @@ class Program
         // var colors = new Dictionary<string, string>();
 
         // light section of export
+        var bestColors = new Dictionary<string, string>();
+        foreach (var color in seedColors)
+        {
+            bestColors.Add((seedColors.IndexOf(color)).ToString(), "#" + color.ToString("X")[2..]);
+        }
+
+        // light section of export
         var colors_light = new Dictionary<string, string>();
         foreach (var property in typeof(Scheme<int>).GetProperties())
         {
             int color = (int)property.GetValue(lightScheme)!;
-            //Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
             colors_light.Add(property.Name, "#" + color.ToString("X")[2..]);
         }
 
@@ -50,9 +87,10 @@ class Program
         foreach (var property in typeof(Scheme<int>).GetProperties())
         {
             int color = (int)property.GetValue(darkScheme)!;
-            //Console.WriteLine($"{property.Name}: #{color.ToString("X")[2..]}");
             colors_dark.Add(property.Name, "#" + color.ToString("X")[2..]);
         }
+
+        string jsonBestColors = JsonConvert.SerializeObject(bestColors);
 
         // light to json
         string jsonLight = JsonConvert.SerializeObject(colors_light);
@@ -60,10 +98,9 @@ class Program
         // dark to json
         string jsonDark = JsonConvert.SerializeObject(colors_dark);
 
-        Console.Write("{\"light\":");
-        Console.WriteLine(jsonLight + ",");
-        Console.WriteLine("\"dark\":");
-        Console.WriteLine(jsonDark);
-        Console.WriteLine("}");
+        Console.WriteLine("{\"bestColors\":"+jsonBestColors + ",");
+        Console.WriteLine("\"seedcolor\":\"#"+seedColor.ToString("X")[2..]+"\",");
+        Console.WriteLine("\"light\":"+jsonLight + ",");
+        Console.WriteLine("\"dark\":"+jsonDark+"}");
     }
 }
