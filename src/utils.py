58a7fc89022283f4ed6d56c8c55234d5cb48ee12
Luis Bocanegra <luisbocanegra17b@gmail.com>
2022-09-15 14:57:51 -0500

    utils: Handle wrong config values individually

--- src/utils.py
+++ src/utils.py
@@ -142,63 +142,99 @@ class Configs():
                 config.read(USER_CONFIG_PATH+CONFIG_FILE)
                 if 'CUSTOM' in config:
                     custom = config['CUSTOM']
-                    c_light = custom.getboolean('light', None)
+                    try:
+                        c_light = custom.getboolean('light', None)
+                    except:
+                        logging.error(
+                            'Value for light must be a boolean, using default None')
+                        c_light = None
+
                     c_file = custom.get('file')
 
                     c_monitor = custom.getint('monitor', 0)
                     if c_monitor < 0:
-                        raise ValueError(
-                            'Value for monitor must be a positive integer')
+                        c_monitor = 0
+                        logging.error(
+                            'Value for monitor must be a positive integer, using default 0')
 
                     c_ncolor = custom.getint('ncolor', 0)
                     if c_ncolor < 0:
-                        raise ValueError(
-                            'Value for ncolor must be a positive integer')
+                        c_ncolor = None
+                        logging.error(
+                            'Value for ncolor must be a positive integer, using default 0')
 
                     c_plugin = custom.get('plugin')
                     c_color = custom.get('color')
                     c_iconslight = custom.get('iconslight')
                     c_iconsdark = custom.get('iconsdark')
-                    c_pywal = custom.getboolean('pywal')
-                    c_pywal_light = custom.getboolean('pywal_light')
+
+                    try:
+                        c_pywal = custom.getboolean('pywal', None)
+                    except:
+                        logging.error(
+                            'Value for pywal must be a boolean, using default None')
+                        c_pywal = None
+
+                    try:
+                        c_pywal_light = custom.getboolean('pywal_light', None)
+                    except:
+                        logging.error(
+                            'Value for pywal_light must be a boolean, using default None')
+                        c_pywal_light = None
 
                     try:
                         c_light_blend_multiplier = custom.getfloat(
                             'light_blend_multiplier')
                     except:
-                        raise ValueError(
-                            'Value for light_blend_multiplier must be a positive number, using default 1.0')
+                        logging.error(
+                            'Value for light_blend_multiplier must be a number between 0.0 and 4.0 , using default 1.0')
+                        c_light_blend_multiplier = 1
 
                     try:
                         c_dark_blend_multiplier = custom.getfloat(
                             'dark_blend_multiplier')
                     except:
-                        raise ValueError(
-                            'Value for dark_blend_multiplier must be a positive number, using default 1.0')
+                        logging.error(
+                            'Value for dark_blend_multiplier must be a number between 0.0 and 4.0, using default 1.0')
+                        c_dark_blend_multiplier = 1
 
                     c_on_change_hook = custom.get('on_change_hook')
-                    c_sierra_breeze_buttons_color = custom.getboolean(
-                        'sierra_breeze_buttons_color')
+
+                    try:
+                        c_sierra_breeze_buttons_color = custom.getboolean(
+                            'sierra_breeze_buttons_color', None)
+                    except:
+                        logging.error(
+                            'Value for sierra_breeze_buttons_color must be a boolean, using default None')
+                        c_sierra_breeze_buttons_color = None
+
                     c_konsole_profile = custom.get('konsole_profile')
 
-                    c_titlebar_opacity = custom.getint('titlebar_opacity', 100)
-                    if c_titlebar_opacity < 0 or c_titlebar_opacity > 100:
-                        raise ValueError(
-                            'Value for titlebar_opacity must be an integer betwritten 0 and 100, using default 100')
+                    c_titlebar_opacity = custom.getint(
+                        'titlebar_opacity', None)
+                    if c_titlebar_opacity != None:
+                        if c_titlebar_opacity < 0 or c_titlebar_opacity > 100:
+                            logging.error(
+                                'Value for titlebar_opacity must be an integer between 0 and 100, using default 100')
+                            c_titlebar_opacity = 100
 
-                    c_toolbar_opacity = custom.getint('toolbar_opacity', 100)
-                    if c_toolbar_opacity < 0 or c_toolbar_opacity > 100:
-                        raise ValueError(
-                            'Value for toolbar_opacity must be an integer betwritten 0 and 100, using default 100')
+                    c_toolbar_opacity = custom.getint('toolbar_opacity', None)
+                    if c_toolbar_opacity != None:
+                        if c_toolbar_opacity < 0 or c_toolbar_opacity > 100:
+                            logging.error(
+                                'Value for toolbar_opacity must be an integer between 0 and 100, using default 100')
+                            c_toolbar_opacity = 100
 
-                    c_konsole_opacity = custom.getint('konsole_opacity', 100)
-
-                    if c_konsole_opacity < 0 or c_konsole_opacity > 100:
-                        raise ValueError(
-                            'Value for konsole_opacity must be an integer betwritten 0 and 100, using default 100')
+                    c_konsole_opacity = custom.getint('konsole_opacity', None)
+                    if c_konsole_opacity != None:
+                        if c_konsole_opacity < 0 or c_konsole_opacity > 100:
+                            logging.error(
+                                'Value for konsole_opacity must be an integer between 0 and 100, using default 100')
+                            c_konsole_opacity = 100
 
             except Exception as e:
                 logging.error(f"Please fix your settings file:\n{e}\n")
+
         if args.dark == True:
             c_light = False
         elif args.light == True:
@@ -229,15 +265,17 @@ class Configs():
 
         if args.monitor != None:
             if args.monitor < 0:
-                logging.error('Value for --monitor must be a positive integer')
-                raise ValueError
+                logging.error(
+                    'Value for --monitor must be a positive integer, using default 0')
+                logging.error
             else:
                 c_monitor = args.monitor
 
         if args.ncolor != None:
             if args.ncolor < 0:
-                logging.error('Value for --ncolor must be a positive integer')
-                raise ValueError
+                logging.error(
+                    'Value for --ncolor must be a positive integer, using default 0')
+                logging.error
             else:
                 c_ncolor = args.ncolor
 
@@ -265,24 +303,23 @@ class Configs():
         if args.titlebar_opacity != None:
             if args.titlebar_opacity < 0 or args.titlebar_opacity > 100:
                 logging.error(
-                    'Value for --sbe-titlebar-opacity must be an integer betwritten 0 and 100')
-                raise ValueError
+                    'Value for --sbe-titlebar-opacity must be an integer between 0 and 100, using default 100')
+
             else:
                 c_titlebar_opacity = args.titlebar_opacity
 
         if args.toolbar_opacity != None:
             if args.toolbar_opacity < 0 or args.toolbar_opacity > 100:
                 logging.error(
-                    'Value for --toolbar-opacity must be an integer betwritten 0 and 100')
-                raise ValueError
+                    'Value for --toolbar-opacity must be an integer between 0 and 100, using default 100')
+
             else:
                 c_toolbar_opacity = args.toolbar_opacity
 
         if args.konsole_opacity != None:
             if args.konsole_opacity < 0 or args.konsole_opacity > 100:
                 logging.error(
-                    'Value for --konsole-opacity must be an integer betwritten 0 and 100')
-                raise ValueError
+                    'Value for --konsole-opacity must be an integer between 0 and 100, using default 100')
             else:
                 c_konsole_opacity = args.konsole_opacity
         elif args.konsole_opacity == None and c_konsole_opacity == None:
@@ -1248,7 +1285,7 @@ def apply_themes(
         plasma_scheme_watcher: Watcher):
     # Print new config after change
     if config_watcher.has_changed:
-        logging.debug(f"New Config: {config_watcher.get_new_value()}")
+        logging.debug(f"Config: {config_watcher.get_new_value()}")
     needs_kwin_reload = False
     group1_watcher.set_value([
         get_config_value(config_watcher.get_new_value(), 'ncolor'),
@@ -1285,7 +1322,7 @@ def apply_themes(
     if wallpaper_watcher.has_changed or group1_watcher.has_changed or wallpaper_modified.has_changed:
         if wallpaper_watcher.has_changed:
             logging.info(
-                f'Source changed: ({wallpaper_new_type}) {wallpaper_new_data}')
+                f'Using source ({wallpaper_new_type}): {wallpaper_new_data}')
         material_colors.set_value(
             get_color_schemes(
                 wallpaper_watcher.get_new_value(),
