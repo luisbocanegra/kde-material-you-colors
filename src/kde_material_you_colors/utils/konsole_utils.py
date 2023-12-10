92bc81db44396835595382a11c2112e2a32ff664
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2023-12-10 12:14:22 -0600

    konsole_utils: apply color scheme to the selected profile by default
    context: https://github.com/luisbocanegra/kde-material-you-colors/issues/156#issuecomment-1703138157
    
    Also reduce qdbus, handle exceptions and clean some things

--- src/kde_material_you_colors/utils/konsole_utils.py
+++ src/kde_material_you_colors/utils/konsole_utils.py
@@ -1,35 +1,31 @@
+import os
 import subprocess
+import configparser
 import logging
+import dbus
 from .color_utils import hex2rgb
 from .math_utils import clip
 from .string_utils import tup2str
-import configparser
-import os
 from .. import settings
+from ..schemeconfigs import ThemeConfig
 
 
-def export_scheme(light=None, pywal_light=None, schemes=None, konsole_opacity=100):
-    if konsole_opacity == None:
+def export_scheme(
+    light=None, pywal_light=None, schemes: ThemeConfig = None, konsole_opacity=100
+):
+    if konsole_opacity is None:
         konsole_opacity = 100
     else:
         konsole_opacity = float(clip(konsole_opacity, 0, 100, 100) / 100)
     # print(f"konsole_opacity: {konsole_opacity}")
-    if pywal_light != None:
-        if pywal_light == True:
-            pywal_colors = schemes.get_wal_light_scheme()
-        else:
-            pywal_colors = schemes.get_wal_dark_scheme()
-    elif light != None:
-        if light == True:
-            pywal_colors = schemes.get_wal_light_scheme()
-        elif light == False:
-            pywal_colors = schemes.get_wal_dark_scheme()
-    else:
-        pywal_colors = schemes.get_wal_dark_scheme()
+    pywal_colors = (
+        schemes.get_wal_light_scheme()
+        if (pywal_light or light)
+        else schemes.get_wal_dark_scheme()
+    )
+
     config = configparser.ConfigParser()
     config.optionxform = str
-    if os.path.exists(settings.KONSOLE_COLOR_SCHEME_PATH):
-        config.read(settings.KONSOLE_COLOR_SCHEME_PATH)
 
     sections = [
         "Background",
@@ -41,19 +37,16 @@ def export_scheme(light=None, pywal_light=None, schemes=None, konsole_opacity=10
         "ForegroundFaint",
         "General",
     ]
-    exp = []
+
     for section in sections:
         if section == "Color":
             for n in range(8):
-                exp.append(str(f"Color{n}"))
-                exp.append(str(f"Color{n}Intense"))
-                exp.append(str(f"Color{n}Faint"))
+                config.add_section(str(f"Color{n}"))
+                config.add_section(str(f"Color{n}Intense"))
+                config.add_section(str(f"Color{n}Faint"))
         else:
-            exp.append(section)
-
-    for section in exp:
-        if not config.has_section(section):
             config.add_section(section)
+
     config["Background"]["Color"] = tup2str(
         hex2rgb(pywal_colors["special"]["background"])
     )
@@ -63,48 +56,21 @@ def export_scheme(light=None, pywal_light=None, schemes=None, konsole_opacity=10
     config["BackgroundFaint"]["Color"] = tup2str(
         hex2rgb(pywal_colors["special"]["backgroundFaint"])
     )
-    config["Color0"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color0"]))
-    config["Color1"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color1"]))
-    config["Color2"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color2"]))
-    config["Color3"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color3"]))
-    config["Color4"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color4"]))
-    config["Color5"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color5"]))
-    config["Color6"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color6"]))
-    config["Color7"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color7"]))
 
-    config["Color0Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color8"])
-    )
-    config["Color1Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color9"])
-    )
-    config["Color2Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color10"])
-    )
-    config["Color3Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color11"])
-    )
-    config["Color4Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color12"])
-    )
-    config["Color5Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color13"])
-    )
-    config["Color6Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color14"])
-    )
-    config["Color7Intense"]["Color"] = tup2str(
-        hex2rgb(pywal_colors["colors"]["color15"])
-    )
+    for i in range(0, 8):
+        config[f"Color{i}"]["Color"] = tup2str(
+            hex2rgb(pywal_colors["colors"][f"color{i}"])
+        )
 
-    config["Color0Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color16"]))
-    config["Color1Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color17"]))
-    config["Color2Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color18"]))
-    config["Color3Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color19"]))
-    config["Color4Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color20"]))
-    config["Color5Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color21"]))
-    config["Color6Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color22"]))
-    config["Color7Faint"]["Color"] = tup2str(hex2rgb(pywal_colors["colors"]["color23"]))
+    for i in range(0, 8):
+        config[f"Color{i}Intense"]["Color"] = tup2str(
+            hex2rgb(pywal_colors["colors"][f"color{i+8}"])
+        )
+
+    for i in range(0, 8):
+        config[f"Color{i}Faint"]["Color"] = tup2str(
+            hex2rgb(pywal_colors["colors"][f"color{i+16}"])
+        )
 
     config["Foreground"]["Color"] = tup2str(
         hex2rgb(pywal_colors["special"]["foreground"])
@@ -119,20 +85,22 @@ def export_scheme(light=None, pywal_light=None, schemes=None, konsole_opacity=10
     config["General"]["Description"] = "MaterialYou"
     config["General"]["Opacity"] = str(konsole_opacity)
 
-    with open(settings.KONSOLE_COLOR_SCHEME_PATH, "w") as configfile:
+    with open(settings.KONSOLE_COLOR_SCHEME_PATH, "w", encoding="utf-8") as configfile:
         config.write(configfile, space_around_delimiters=False)
 
     config["General"]["Description"] = "MaterialYouAlt"
 
-    with open(settings.KONSOLE_COLOR_SCHEME_ALT_PATH, "w") as configfile:
+    with open(
+        settings.KONSOLE_COLOR_SCHEME_ALT_PATH, "w", encoding="utf-8"
+    ) as configfile:
         config.write(configfile, space_around_delimiters=False)
 
 
 def make_mirror_profile(profile=None):
-    if profile != None:
+    if profile is not None:
         profile_path = settings.KONSOLE_DIR + profile + ".profile"
         if os.path.exists(profile_path):
-            logging.debug(f"konsole: mirror profile ({profile})")
+            logging.info(f"Konsole profile: ({profile})")
             subprocess.check_output(
                 "cp -f '" + profile_path + "' " + settings.KONSOLE_TEMP_PROFILE,
                 shell=True,
@@ -143,11 +111,13 @@ def make_mirror_profile(profile=None):
             if os.path.exists(profile_path):
                 try:
                     profile.read(profile_path)
-                    if "Appearance" in profile:
-                        if profile["Appearance"]["ColorScheme"] != "MaterialYou":
-                            profile["Appearance"]["ColorScheme"] = "MaterialYou"
-                            with open(profile_path, "w") as configfile:
-                                profile.write(configfile, space_around_delimiters=False)
+                    if "Appearance" not in profile:
+                        profile.add_section("Appearance")
+
+                    if profile["Appearance"]["ColorScheme"] != "MaterialYou":
+                        profile["Appearance"]["ColorScheme"] = "MaterialYou"
+                        with open(profile_path, "w", encoding="utf-8") as configfile:
+                            profile.write(configfile, space_around_delimiters=False)
                 except Exception as e:
                     logging.error(f"Error applying Konsole profile:\n{e}")
 
@@ -157,77 +127,77 @@ def make_mirror_profile(profile=None):
             if os.path.exists(settings.KONSOLE_TEMP_PROFILE):
                 try:
                     profile.read(settings.KONSOLE_TEMP_PROFILE)
-                    if "Appearance" in profile:
-                        profile["Appearance"]["ColorScheme"] = "MaterialYouAlt"
-                        profile["General"]["Name"] = "TempMyou"
+                    if "Appearance" not in profile:
+                        profile.add_section("Appearance")
+                    profile["Appearance"]["ColorScheme"] = "MaterialYouAlt"
+                    profile["General"]["Name"] = "TempMyou"
                 except Exception as e:
                     logging.error(f"Error applying Konsole profile:\n{e}")
-                with open(settings.KONSOLE_TEMP_PROFILE, "w") as configfile:
+                with open(
+                    settings.KONSOLE_TEMP_PROFILE, "w", encoding="utf-8"
+                ) as configfile:
                     profile.write(configfile, space_around_delimiters=False)
 
 
 def reload_profile(profile=None):
-    if profile != None:
-        logging.info(f"konsole: reload profile ({profile})")
-        konsole_dbus_services = (
-            subprocess.check_output(
-                "qdbus org.kde.konsole*", shell=True, universal_newlines=True
-            )
-            .strip()
-            .splitlines()
-        )
+    if profile is not None:
+        bus = dbus.SessionBus()
+        konsole_dbus_services = bus.list_names() or []
+        # Get konsole instances (windows)
+        konsole_dbus_services = [
+            service for service in konsole_dbus_services if "org.kde.konsole" in service
+        ]
+
         if konsole_dbus_services:
+            logging.debug(
+                f"Konsole services (windows) running ({len(konsole_dbus_services)}):"
+            )
             for service in konsole_dbus_services:
                 try:
-                    konsole_sessions = (
-                        subprocess.check_output(
-                            "qdbus " + service + " | grep 'Sessions/'",
-                            shell=True,
-                            universal_newlines=True,
-                        )
-                        .strip()
-                        .splitlines()
+                    # get open sessions (tabs and splits)
+                    cmd = ["qdbus", service]
+                    result = subprocess.run(
+                        cmd,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        text=True,
+                        check=True,
                     )
-                    for session in konsole_sessions:
-                        # print(f"service: {service} -> {session}")
-                        current_profile = subprocess.check_output(
-                            "qdbus "
-                            + service
-                            + " "
-                            + session
-                            + " org.kde.konsole.Session.profile",
-                            shell=True,
-                            universal_newlines=True,
-                        ).strip()
-                        # print(current_profile)
-                        if current_profile == profile:
-                            subprocess.check_output(
-                                "qdbus "
-                                + service
-                                + " "
-                                + session
-                                + " org.kde.konsole.Session.setProfile 'TempMyou'",
-                                shell=True,
-                            )
-                        else:
-                            subprocess.check_output(
-                                "qdbus "
-                                + service
-                                + " "
-                                + session
-                                + " org.kde.konsole.Session.setProfile '"
-                                + profile
-                                + "'",
-                                shell=True,
-                            )
-                except:
+
+                    sessions = [
+                        line
+                        for line in result.stdout.splitlines()
+                        if line.startswith("/Sessions/")
+                    ]
+                    logging.debug(f"{service} ({len(sessions)} sessions)")
+
+                    # reload colors by switching profiles
+                    for session in sessions:
+                        session_obj = bus.get_object(service, session)
+                        session_iface = dbus.Interface(
+                            session_obj, "org.kde.konsole.Session"
+                        )
+                        # logging.debug(f"{service}, {session} reading profile...")
+                        current_profile = session_iface.profile()
+                        # logging.debug(f"{current_profile}")
+                        new_profile = (
+                            "TempMyou" if profile == current_profile else profile
+                        )
+
+                        # logging.debug(f"{service}, {session} setting profile...")
+                        session_iface.setProfile(new_profile)
+                        # logging.debug("done")
+
+                except dbus.exceptions.DBusException:
                     pass
+                except (FileNotFoundError, subprocess.CalledProcessError) as e:
+                    logging.error(f"{e}")
 
 
 def apply_color_scheme(
     light=None, pywal_light=None, schemes=None, profile=None, konsole_opacity=None
 ):
-    if profile != None:
+    if profile is not None:
         profile_path = settings.KONSOLE_DIR + profile + ".profile"
         if os.path.exists(profile_path):
             export_scheme(light, pywal_light, schemes, konsole_opacity)
